ad31d1a72311a7dc72b85317019d5accb40e9655
larissafeital <larissa.feital@cortex-intelligence.com>
2026-06-15 15:04:30 -0300

    chore: remover arquivos obsoletos (planejamento e update.sh)

--- update.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-git add index.html qa-data.js
-git commit -m "update: novos dados QA"
-git push
-echo "✅ Painel atualizado: https://larissafeital.github.io/qa-painel-cortex/"
